51a2f20fa15aaaa7479f7e550868790669999948
Alex Siegberg and Mike Lofgren <alex@>
2019-07-03 15:05:12 -0700

    add APIs to generate random cat facts and dog pics

---
+++ src/dogOut.js
@@ -0,0 +1,18 @@
+class DogPics{
+  getDog() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = process.env.DOG_KEY;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        }else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+module.exports = DogPics;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
-import { sum } from './sample-class.js';
+const CatFacts = require('./catOut')
+const DogPics = require('./dogOut')
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
+  $('#catScraps').submit(function(event) {
+    event.preventDefault();
 
+    let catService = new CatFacts();
+    let catPromise = catService.getCat();
+
+    catPromise.then(function(response) {
+      let body = JSON.parse(response);
+      const randomNum = Math.floor(Math.random() * body.all.length) + 0;
+      $('.catPut').text(body.all[randomNum].text);
+    }, function(error) {
+      $('.catPut').text('There was an error dummy!');
+    });
+
+    let dogService = new DogPics();
+    let dogPromise = dogService.getDog();
+
+    dogPromise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.dogPut').html(`<img src="${body.url}">`);
+    }, function(error) {
+      $('.dogPut').text('There was an error dummy!');
+    });
+
+
+
+
+  });
 });
